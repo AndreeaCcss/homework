f43bb5234b5434e57225032a357756fa39357043
andreeaCcss <andreeaccss@gmail.com>
2019-08-24 09:13:13 +0200

    Implemented Step 1

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Provider } from "react-redux";
+import store from "./store";
+
+const data = [
+  {
+    name: "Ivel Z3",
+    manufacturer: "Ivasim",
+    year: 1969,
+    origin: "Croatia"
+  },
+  {
+    name: "Bally Astrocade",
+    manufacturer: "Bally Consumer Products",
+    year: 1977,
+    origin: "USA"
+  },
+  {
+    name: "Sord M200 Smart Home Computer",
+    manufacturer: "Sord Computer Corporation",
+    year: 1971,
+    origin: "Japan"
+  },
+  {
+    name: "Commodore 64",
+    manufacturer: "Commodore",
+    year: 1982,
+    origin: "USA"
+  }
+];
 
 function App() {
+  console.log(data.map(model => model.name));
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <select>
+          {data.map(model => (
+            <option value={model.name}>
+              {model.name}, ({model.year}}
+            </option>
+          ))}
+        </select>
+      </div>
+    </Provider>
   );
 }
+// {props.pizzas.map(pizza => (
+//   <li key={pizza.id} onClick={() => props.selectPizza(pizza.id)}>
+//     {pizza.name}
+//   </li>
+// ))}
 
 export default App;
